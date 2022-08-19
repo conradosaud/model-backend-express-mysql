7cf962ab100e0c4e3e50e7cec2c8e7eb552dcc87
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2022-08-19 13:50:51 -0300

    Versão simples do banck-end

--- index.js
+++ index.js
@@ -14,11 +14,123 @@ app.use(function(req, res, next) {
     next();
 });
 
-// ROTAS DO EXPRESS
-const rotas = require("./rotas/rotas");
-app.use( rotas );
 
-// Inicia a aplica~çao
+// Conexão com o banco de dados
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "senha_do_banco",
+  database: "nome_do_banco"
+});
+
+/* ######### ROTAS DO EXPRESS ######### */
+
+// ### Rotas de FILMES
+
+// Busca todos os filmes
+app.get("/filmes", ( req, res ) => {
+
+    const query = ` SELECT * FROM filmes ORDER BY id DESC ; `;
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+// Busca os filmes através do título
+app.get("/filmes/busca_titulo/:titulo", ( req, res ) => {
+
+    const titulo = req.params.titulo;
+
+    const query = ` SELECT * FROM filmes WHERE titulo LIKE "%${titulo}%" ; `;
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+// Insere um filme no banco
+app.post("/filmes", ( req, res ) => {
+
+    const titulo = req.body.titulo;
+    const sinopse = req.body.sinopse;
+
+    const query = ` INSERT INTO filmes ( titulo, sinopse ) VALUES ( "${titulo}" , "${sinopse}"  ); `;
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+// Insere um filme do banco através do ID informado
+app.delete("/filmes/:id", ( req, res ) => {
+
+    const id = req.parmas.id;
+
+    const query = ` DELETE FROM filmes WHERE id = ${id}; `;
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+// ### Rotas de USUÁRIOS
+
+// Insere um usuário no banco
+app.post("/usuarios", ( req, res ) => {
+
+    const usuario = req.body.usuario;
+    const nome = req.body.nome;
+    const senha = req.body.senha;
+
+    const query = ` INSERT INTO usuarios ( usuario, nome, senha ) VALUES ( "${usuario}", "${nome}" , "${senha}"  ); `;
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+// Autentica o usuário no banco
+app.post("/usuarios/autentica", ( req, res ) => {
+
+    const usuario = req.body.usuario;
+    const senha = req.body.senha;
+
+    const query = ` SELECT id, nome FROM usuarios WHERE usuario = "${usuario}" AND senha = "${senha}" ; `
+    connection.query( query, (err, results) => {
+        if( results ){
+            res.send(results)
+        }else{
+            res.send( err )
+        }
+    });
+
+});
+
+
+// Inicia a aplicão
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- rotas/rotasUsuarios.js
+++
@@ -1,8 +0,0 @@
-const rotas = require("express").Router();
-const caminho = ( metodo ) => require("../controlador/usuarios/"+metodo);
-
-// // POST
-rotas.post("/", caminho("post").Insere );
-rotas.post("/autentica", caminho("post").Autentica );
-
-module.exports = rotas;
